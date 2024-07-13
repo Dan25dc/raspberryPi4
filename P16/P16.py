56a89030fd81b44bf1f2372528272d049eef9396
Dan25dc <dac.25a@gmail.com>
2024-07-13 08:30:59 -0600

    correccion p16

--- P16/P16.py
+++ P16/P16.py
@@ -16,6 +16,8 @@ GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1E, GPIO.OUT)
 
+GPIO.output(Motor1E,GPIO.LOW)
+
 def avanzar():
     GPIO.output(Motor1A,GPIO.HIGH)
     GPIO.output(Motor1B,GPIO.LOW)
